73ce58de9acf2e311dfcfb5a414fbbfef7a50eeb
Jeremy Kazimer <jeremykazimer@gmail.com>
2025-08-30 11:56:29 -0400

    Raise better error

--- pyreads/_http.py
+++ pyreads/_http.py
@@ -1,6 +1,6 @@
 """Internal HTTP utilities for PyReads."""
 
-import httpx
+from httpx import Client, HTTPStatusError
 
 
 def _format_goodreads_url(user_id: int, page: int = 1) -> str:
@@ -10,11 +10,15 @@ def _format_goodreads_url(user_id: int, page: int = 1) -> str:
     return f"https://www.goodreads.com/review/list/{user_id}?page={page}&shelf=read"
 
 
-def _fetch_html(client: httpx.Client, url: str) -> str:
+def _fetch_html(client: Client, url: str) -> str:
     """
-    Sends a GET request and returns the response text, or raises ValueError on error.
+    Sends a GET request and returns the response text
     """
     response = client.get(url)
     if response.status_code == 200:
         return response.text
-    raise ValueError(f"{response.status_code} Error: {response.url}")
+
+    err = f"{response.status_code} Error: {response.url}"
+    raise HTTPStatusError(
+        message=err, request=response.request, response=response
+    )

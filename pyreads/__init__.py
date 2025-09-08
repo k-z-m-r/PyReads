66b723072dffe2896f5cc0eb7cc4348d146af2fd
Jeremy Kazimer <155553211+jeremy-kazimer@users.noreply.github.com>
2025-09-07 23:56:30 -0400

    Reduce version

--- pyreads/__init__.py
+++ pyreads/__init__.py
@@ -3,6 +3,6 @@
 from .core import fetch_library
 from .models import Book, Library
 
-__version__ = "0.4.0"
+__version__ = "0.3.1"
 __author__ = "Jeremy Kazimer"
 __all__ = ["Book", "Library", "fetch_library"]

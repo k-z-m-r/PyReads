c2a56ccc9202979371e2d396ad1c5f124b0b38b4
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2025-09-26 23:37:16 -0400

    Bump version to 0.3.4

--- pyreads/__init__.py
+++ pyreads/__init__.py
@@ -3,6 +3,6 @@
 from .core import fetch_library
 from .models import Book, Library
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Jeremy Kazimer"
 __all__ = ["Book", "Library", "fetch_library"]
